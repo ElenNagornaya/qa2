05e10ce205df7f3ca8bdd86ca9e06c9215955d67
Elena Nagornaya <elen_nagornaya@mail.ru>
2023-11-04 19:32:51 +0300

    changed main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        
+       int initialBalance = 500;
+       int accountReplenishment = 1400;
+       int bonus = 0;
+
+        if (accountReplenishment > 1000) {
+            bonus = (accountReplenishment / 100);
+        }
+
+       int balance = initialBalance + accountReplenishment + bonus;
+
+        System.out.println("Сумма баланса: " + balance);
+        System.out.println("Начислено бонусов: " + bonus);
+
+
     }
 }
